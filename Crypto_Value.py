895e515dba2384988ac0e39e58ba7a9947f3232f
Navin Karthikeyan <52325703+navinkarthikeyan@users.noreply.github.com>
2023-04-27 22:24:04 +0530

    Add files via upload

---
+++ Crypto_Value.py
@@ -0,0 +1,21 @@
+import requests
+import csv
+url = "http://api.coincap.io/v2/assets"
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+myjson = response.json()
+our_data = []
+csvheader = ['Symbol','Name','Price in USD']
+for x in myjson['data']:
+    listing = [x['symbol'],x['name'],x['priceUsd']]
+    our_data.append(listing) 
+    
+with open('crypto.csv','w',encoding='UTF8',newline = '') as f:
+    writer = csv.writer(f)
+    
+    writer.writerow(csvheader)
+    writer.writerows(our_data)
+print('done')
